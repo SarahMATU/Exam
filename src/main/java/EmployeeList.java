47e32f7be961f0347e175020adef980bd2e4d2f5
Sarah Mitchell <g00391704@atu.ie>
2022-12-07 12:42:05 +0000

    Started Exam

--- src/main/java/EmployeeList.java
+++ src/main/java/EmployeeList.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class EmployeeList {
 
+    ArrayList<Employee> newEmployee = new ArrayList<Employee>();
 
+    public List<Employee> getEmployee(){
+    return newEmployee;
+}
+
+    public void addEmployees(Employee obj){
+
+            Employee newEmp = new Employee(obj.getEmName(), obj.getEmPPS());
+            newEmployee.add(newEmp);
+
+    }
+
+    public void getListSize(){
+
+        System.out.println("Current Employee Count is: "+ newEmployee.size());
+
+    }
 
 }
--- src/test/java/EmployeeTestFail.java
+++ src/test/java/EmployeeTestFail.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 
-
-
 public class EmployeeTestFail {
 
     @BeforeEach
@@ -12,21 +10,28 @@ public class EmployeeTestFail {
 
     }
 
-    void testEmployee(){
-        Employee employeeNew = new Employee("Sarah", "65432W");
-    }
-
     @Test
     void testConstructor(String emName, String emPPS){
-        Employee employeeNew =  new Employee("Sarah", "65432W");
-        Assertions.assertEquals("Darah", employeeNew.getEmName());
-        Assertions.assertEquals("6231W", employeeNew.getEmPPS());
+        Employee employeeNew =  new Employee("Darah", "65432W");
+        Assertions.assertEquals("Sarah","Darah",  employeeNew.getEmName());
+        Assertions.assertEquals("65432W", "65432T", employeeNew.getEmPPS());
 
     }
 
     @Test
     void testNameFail(){
-        Employee employeeNew = new Employee("Darah", "65432W");
+        Employee employeeNew =  new Employee("Darah", "65432W");
+        Assertions.assertEquals("Darah", "Sarah", employeeNew.getEmName());
+    }
+
+    @Test
+    void testPPSFail(){
+        Employee employeeNew =  new Employee("Sarah", "65432W");
+        Assertions.assertEquals("65432T", "65432W", employeeNew.getEmPPS());
+    }
+
+    @AfterEach
+    void teardown(){
 
     }
 
